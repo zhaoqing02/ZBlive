95c0bf46d8d4f4dbeddf030664eee9371ec4da37
xihi <xuhui0607@gmail.com>
2024-10-13 20:26:15 +0800

    update

--- app/src/main/java/com/lizongying/mytv0/requests/HttpClient.kt
+++ app/src/main/java/com/lizongying/mytv0/requests/HttpClient.kt
@@ -21,8 +21,8 @@ import javax.net.ssl.X509TrustManager
 
 object HttpClient {
     const val TAG = "HttpClient"
-    private const val HOST = "https://www.gitlink.org.cn/lizongying/my-tv-0/raw/"
-    const val DOWNLOAD_HOST = "https://www.gitlink.org.cn/lizongying/my-tv-0/releases/download/"
+    private const val HOST = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/my-tv-0/"
+    const val DOWNLOAD_HOST = "https://mirror.ghproxy.com/https://github.com/xisohi/my-tv-0/releases/download/"
 
     val okHttpClient: OkHttpClient by lazy {
         getUnsafeOkHttpClient()
