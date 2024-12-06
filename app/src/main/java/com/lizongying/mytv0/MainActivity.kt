af259e47d67d18b953fd9531a7792bcc715f4af8
xihi <xuhui0607@gmail.com>
2024-12-06 12:11:59 +0800

    修改应用语言为简体中文

--- app/src/main/java/com/lizongying/mytv0/MainActivity.kt
+++ app/src/main/java/com/lizongying/mytv0/MainActivity.kt
@@ -744,7 +744,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun attachBaseContext(base: Context) {
         try {
-            val locale = Locale.TRADITIONAL_CHINESE
+            val locale = Locale.SIMPLIFIED_CHINESE
             val config = Configuration()
             config.setLocale(locale)
             super.attachBaseContext(
